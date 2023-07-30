5cc1bb68590867ea67bdb2d08fdb62180051e114
jramos5883 <jramos5883@gmail.com>
2023-07-29 17:04:38 -0700

    Doing well, feeling better

--- rnp-app/app/index.jsx
+++ rnp-app/app/index.jsx
@@ -1,21 +1,35 @@
-import { useState } from 'react';
-import { View, ScrollView, SafeAreaView, Text } from 'react-native'
-import {Stack, useRouter } from 'expo-router';
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
-import { COLORS, icons, images, SIZES } from '../constants';
-import { 
-    Nearbyjobs, 
-    Popularjobs, 
-    ScreenHeaderBtn, 
-    Welcome 
-} from '../components'
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 export default function Home() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <SafeAreaView stlye={{flex:1, backgroundcolor: COLORS.lightWhite}}>
-          <Stack.Screen options={{headerStyle: {backgroundcolor: COLORS.lightWhite},}} />
-        </SafeAreaView>
-    )
-} 
+  // 30 mins in
+
+  return (
+    <SafeAreaView stlye={{ flex: 1, backgroundcolor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundcolor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+    </SafeAreaView>
+  );
+}
